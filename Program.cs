b8c91a853445b212ea5d6ebdead905111d623c84
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-05-15 19:54:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira seu nome: ");
+            String nome = Console.ReadLine();
+            Console.ReadLine();
+            Console.Write("Insira seu sobrenome: ");
+            String sobrenome = Console.ReadLine();
+            Console.ReadLine();
+            Console.WriteLine(nome+ " " +sobrenome);
+            Console.WriteLine($"{sobrenome.ToUpper()} {nome}");
+
+        }
+    }
+}
